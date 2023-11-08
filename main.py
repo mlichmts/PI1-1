ba9bea3b5846f34d2cc1fd3de1a489c569c6ff98
Matúš Mlích <mlichmatus0@gmail.com>
2023-11-08 10:39:03 +0100

    update main.py - change sucet to sucin

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-a = int(input("zadaj cislo a: "))
-b = int(input("zadaj cislo b: "))
-print("sucin je:", a * b)
-print("sucet je:", a + b)
-print("hello World")
+import tkinter
+
+canvas = tkinter.Canvas()
+canvas.pack()
+x = 2
+y = 2
+dlzka = 50
+canvas.create_rectangle(x, y, x + dlzka, y + dlzka, fill="red")
+canvas.create_rectangle(x + dlzka, y, x + (2 * dlzka), y + dlzka)
+canvas.create_rectangle(x + (2*dlzka), y, x + (3 * dlzka), y + dlzka)
+canvas.create_oval(x + dlzka, y + dlzka,  x + (2 * dlzka), y + (2 * dlzka))
+canvas.create_oval(x + dlzka, y + (2 * dlzka), x + (2 * dlzka), y + (3 * dlzka))
+canvas.create_rectangle(x, y + (3 * dlzka), x + (2 * dlzka), y + (4 * dlzka))
+canvas.create_rectangle(x, y + (3 * dlzka), x + dlzka, y + (4 * dlzka))
+canvas.create_rectangle(x + (2 * dlzka), y + (3 * dlzka), x + (3 * dlzka), y + (4 * dlzka))
+canvas.mainloop()
