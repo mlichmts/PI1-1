e3e1ab1cf891c692ef7b53f780ea4ec62c807a1c
mlich <mlichmatus0@gmail.com>
2023-11-19 20:50:48 +0100

    upravenie domácej ulohy "vypracovanie.py"

--- 11-DU/vypracovanie.py
+++ 11-DU/vypracovanie.py
@@ -2,9 +2,9 @@ import tkinter
 
 canvas = tkinter.Canvas()
 canvas.pack()
-x = 5
-y = 5
-d = 5
+x = 25
+y = 50
+d = 25
 f ="red"
 
 
@@ -27,7 +27,7 @@ canvas.create_rectangle(x+4*d, y+4*d, x+5*d, y+5*d, fill=f)
 canvas.create_rectangle(x+4*d, y+5*d, x+5*d, y+6*d, fill=f)
 canvas.create_rectangle(x+4*d, y+6*d, x+5*d, y+7*d, fill=f)
 #písmeno M
-x = 8*d
+x = 8*d+x
 f = "blue"
 canvas.create_rectangle(x, y+6*d, x+d, y+7*d, fill=f)
 canvas.create_rectangle(x, y+5*d, x+d, y+6*d, fill=f)
@@ -48,7 +48,7 @@ canvas.create_rectangle(x+2*d, y+3*d, x+3*d, y+4*d, fill=f)
 canvas.create_rectangle(x+d, y+3*d, x+2*d, y+4*d, fill=f)
 canvas.create_rectangle(x+3*d, y+3*d, x+4*d, y+4*d, fill=f)
 #písmeno A
-x = 16*d
+x = 8*d+x
 f = "yellow"
 canvas.create_rectangle(x, y, x+d, y+d, fill=f)
 canvas.create_rectangle(x+d, y, x+2*d, y+d, fill=f)
@@ -62,7 +62,7 @@ canvas.create_rectangle(x+2*d, y+4*d, x+3*d, y+5*d, fill=f)
 canvas.create_rectangle(x+2*d, y+5*d, x+3*d, y+6*d, fill=f)
 canvas.create_rectangle(x+2*d, y+6*d, x+3*d, y+7*d, fill=f)
 #písmeno T
-x = 24*d
+x = 8*d+x
 f = "pink"
 canvas.create_rectangle(x, y, x+d, y+d, fill=f)
 canvas.create_rectangle(x, y+d, x+d, y+2*d, fill=f)
@@ -80,7 +80,7 @@ canvas.create_rectangle(x+4*d, y+2*d, x+5*d, y+3*d, fill=f)
 canvas.create_rectangle(x+4*d, y+d, x+5*d, y+2*d, fill=f)
 canvas.create_rectangle(x+4*d, y, x+5*d, y+d, fill=f)
 #písmeno U
-x = 32*d
+x = 8*d+x
 f = "orange"
 canvas.create_rectangle(x+d, y, x+2*d, y+d, fill=f)
 canvas.create_rectangle(x+2*d, y, x+3*d, y+d, fill=f)
